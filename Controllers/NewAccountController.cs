f9ef21af06faceddf24254c53719a0845c910aed
ToSzi <t.horvath@rgu.ac.uk>
2017-08-17 09:58:16 +0100

    Some GUI enhancements such as validation for opening new CA and changed Home link based on user being logged in or not

--- Controllers/NewAccountController.cs
+++ Controllers/NewAccountController.cs
@@ -68,31 +68,39 @@ namespace MScBank.Controllers
         [HttpPost]
         public ActionResult CreateCurrentAccount(CurrentAccount account) {
 
-            var uId = User.Identity.GetUserId();     
+            if (!ModelState.IsValid) {
 
-            using (var _context = new ApplicationDbContext()) {
-
-                var accountId = 0;
-
-                var existingAccounts = _context.Accounts.Where(a => a.Id == accountId);
-                var highestAccountId = _context.Accounts.Max(a => a.Id);
-                accountId = highestAccountId++;
-
-                var newCurrentAccount = new CurrentAccount {
-
-                    Balance = account.Balance,
-                    SortCode = "12-34-56",
-                    Transactions = new List<Transaction>(),
-                    AccountNumber = highestAccountId.ToString().PadLeft(8, '0'),
-                    ApplicationUserId = uId
-                };
-
-                _context.Accounts.Add(newCurrentAccount);
-                _context.SaveChanges();
-
-               
+                return View("DebitAccountForm");
             }
-                
+
+            try {
+                var uId = User.Identity.GetUserId();
+
+                using (var _context = new ApplicationDbContext()) {
+
+                    var accountId = 0;
+
+                    var existingAccounts = _context.Accounts.Where(a => a.Id == accountId);
+                    var highestAccountId = _context.Accounts.Max(a => a.Id);
+                    accountId = highestAccountId++;
+
+                    var newCurrentAccount = new CurrentAccount {
+
+                        Balance = account.Balance,
+                        SortCode = "12-34-56",
+                        Transactions = new List<Transaction>(),
+                        AccountNumber = highestAccountId.ToString().PadLeft(8, '0'),
+                        ApplicationUserId = uId
+                    };
+
+                    _context.Accounts.Add(newCurrentAccount);
+                    _context.SaveChanges();
+                }
+            } catch (FormatException ex) {
+
+                return View("DebitAccountForm");
+            }
+
             return RedirectToAction("Index", "LoggedIn");
         }
 
