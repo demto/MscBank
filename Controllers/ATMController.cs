6d0494333579c3e4ef2fc9c87e56adc4d0328d3b
ToSzi <t.horvath@rgu.ac.uk>
2017-08-20 10:14:14 +0100

    Started with ATM control and added Index View

---
+++ Controllers/ATMController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MScBank.Controllers
+{
+    public class ATMController : Controller
+    {
+        // GET: ATM
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
